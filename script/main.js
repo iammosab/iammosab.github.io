3a9665dfface81be21b3617641d46aac17fbeba9
Moussaab Waziz <wazizmosab@gmail.com>
2019-06-23 00:35:52 +0100

    Commited with pride

--- script/main.js
+++ script/main.js
@@ -1,11 +1,22 @@
 
 
-$.ajax({
-    url: "https://script.google.com/macros/s/AKfycbwrn62-lhHReM5eZxnBDfThO5DPmb0mllq_XSQP0TRA3eI-cWnR/exec", 
-    type:"POST",
-    data:{action:"readQuotes"},
-    success: function(result){
-        result.forEach(element => {
-            console.log(element);
-        });
-  }});
+
+
+  $(document).ready(function(){
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbwrn62-lhHReM5eZxnBDfThO5DPmb0mllq_XSQP0TRA3eI-cWnR/exec", 
+        type:"POST",
+        data:{action:"readQuotes"},
+        success: function(result){
+            var text = "";
+            result.forEach(element => {
+                text +="<tr>";
+                text +="<td>"+element.id+"</td>";
+                text +="<td>"+element.quote+"</td>";
+                text +="<td>"+element.author+"</td>";
+                text +="<td>"+element.categorie+"</td>";
+                text +="</tr>";
+            });
+            $("#tbody").html(text);
+      }});
+  });
